6b1274cc221e626285e227840bc41603d2199535
amongusdorrs1-art <amongusdorrs1@gmail.com>
2026-06-05 15:08:15 +0200

    Update MixinInGameHud.java

--- src/main/java/me/fireclient/mixins/MixinInGameHud.java
+++ src/main/java/me/fireclient/mixins/MixinInGameHud.java
@@ -1,1 +1,18 @@
+package me.fireclient.mixins;
 
+import me.fireclient.gui.ClickGuiScreen;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class MixinInGameHud {
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void onRender(CallbackInfo ci) {
+        if (ClickGuiScreen.isHudHidden()) {
+            ci.cancel();
+        }
+    }
+}
